606ae22a67bc46e451d762a3914ec27c8b535e1a
Y-v-a-i-n <107935034+Y-v-a-i-n@users.noreply.github.com>
2022-06-25 16:53:07 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Crypt
+ A module to encrypt/decrypt plain text
---
+++ test/main.js
@@ -0,0 +1,7 @@
+const crypt=require("../index.js");
+var encryptedMessage=crypt.encrypt("theKey","theMessage")
+console.log(encryptedMessage)
+//output : 㒐⼠ⷄ⋤ⷄ㐜㐜⯴⺬ⷄ
+var decryptedMessage=crypt.decrypt("theKey",encryptedMessage)
+console.log(decryptedMessage)
+//output : theMessage
